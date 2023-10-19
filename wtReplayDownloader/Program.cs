98e2ce5e2425aa1d2552b0f2e2b6c1f43635ed73
Дмитрий <dimaru.97@mail.ru>
2023-10-20 00:23:38 +0300

    Добавьте файлы проекта.

---
+++ wtReplayDownloader/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.Write("Link to last part of replay: ");
+string? link = Console.ReadLine();
+Replay replay;
+if (!Replay.TryParse(link, out replay))
+{
+    Console.WriteLine("Incorrect link!");
+    Console.ReadKey();
+    return;
+}
+
+Directory.CreateDirectory(replay.Hash);
+for (int i = 0; i < replay.CountOfParts; i++)
+{
+    
+    Console.Write("Part #{0} downloading...", i);
+    using (var client = new HttpClient())
+    {
+        using (var s = client.GetStreamAsync(replay.GetURL(i)))
+        {
+            using (var fs = new FileStream(replay.GetFileName(i), FileMode.OpenOrCreate))
+            {
+                try
+                {
+                    s.Result.CopyTo(fs);
+                    Console.WriteLine(" - Done!");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(" - Error!\n" + ex.Message);
+                    Console.ReadKey();
+                    return;
+                }
+            }
+        }
+    }
+}
+
+Console.WriteLine("Download has been finished.");
+if (Directory.Exists(Environment.CurrentDirectory + "\\" + replay.Hash))
+    System.Diagnostics.Process.Start("explorer.exe", Environment.CurrentDirectory + "\\" + replay.Hash);
+Console.ReadKey();
---
+++ wtReplayDownloader/Replay.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.RegularExpressions;
+
+struct Replay
+{
+    static string pattern = """
+(https://wt-game-replays\.warthunder\.com)/([\d\w]+)/(\d{4})\.wrpl
+""";
+
+    public string Hash;
+    public int CountOfParts;
+    public string Address;
+
+    static public bool TryParse(string? param, out Replay replay)
+    {
+        replay = new Replay();
+        if (param == null)
+            return false;
+
+        var matches = Regex.Matches(param, pattern);
+        if (matches.Count == 0)
+            return false;
+
+        replay.Address = matches[0].Groups[1].Value;
+        replay.Hash = matches[0].Groups[2].Value;
+        replay.CountOfParts = int.Parse(matches[0].Groups[3].Value) + 1;
+        return true;
+    }
+
+    public string GetURL(int part)
+    {
+        return Address + "/" + Hash + "/" + part.ToString("D4") + ".wrpl";
+    }
+
+    public string GetFileName(int part)
+    {
+        return Hash + "\\" + part.ToString("D4") + ".wrpl";
+    }
+}
